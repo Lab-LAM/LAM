c884f7d017ab59dbef9dc1c79ca4b52cb206a3fc
Lab-LAM <122761329+Lab-LAM@users.noreply.github.com>
2023-02-14 17:09:26 -0300

    att

--- src/pages/mission/mission.js
+++ src/pages/mission/mission.js
@@ -1,29 +1,19 @@
 import React from "react";
-import './mission.css';
+import './projects.css';
 import Title from "../../components/Title";
-import Conteudo from "../../components/Conteudo";
+import Projeto from "../../components/Projeto"
 
 const mission = ()=>{
     return(
-        <div>
-            <div className="capa" style={{marginBottom:20}}></div>
-            <Title title="Sobre Nós"/>
-            <Conteudo content="O Laboratório de Análises Moleculares (LAM) fica localizado na Incubadora de empresas da UFT-Palmas, coordenado pelo professor Horllys Gomes Barreto, o laboratório é voltado para o estudo do DNA e agrupa dois laboratórios: um no Câmpus de Gurupi e outro no Câmpus de Palmas."/>
-            <Conteudo content="Em Palmas o LAM faz parte do complexo de laboratórios da saúde e é voltado para a área humana. O mesmo diagnostica doenças como zica, dengue, H1N1, leishmaniose, entre outras. Barreto explica ainda, que estão sendo desenvolvidas tecnologias novas para diagnósticos com maior precisão, agilidade e melhor relação custo/benefício."/>
-
-            <Title title="DNA Technologies"/>
-            <Conteudo content="A DNA Technologies é uma empresa que tem como missão assegurar confiança, precisão e agilidade na realização de diagnósticos moleculares, prezando pelo bom atendimento e comprometimento com os clientes, colaboradores e parceiros. Ela oferece produtos como os kits para extração de ácidos nucleicos e diagnósticos moleculares; serviços de desenho de primers, extração de ácidos nucleicos, PCR em tempo real, diagnósticos moleculares e sequenciamento, além de cursos de capacitação voltados ao estudo de DNA, para atender tanto a academia quanto aos laboratórios especializados e desenvolvimento de produtos ''tecnologias in house'', voltadas para diagnósticos moleculares."/>
-            <Conteudo content="O professor Horllys Barreto afirma que a empresa pode contribuir para a realização de diagnósticos no Estado. “Hoje, toda a parte de estudo e análise de DNA realizada no Tocantins é terceirizada para fora do Estado. A proposta é prestar esse serviço aqui dentro, com precisão, agilidade e custo mais baixo”."/>
-            
-            {/* <div className="equipe" style={{color:'white'}}>
-                <Title title="Equipe"/>
-                <div style={{display:'flex', justifyContent:'center', marginTop:50}}>
-                    {data.map((item, index)=> <Equipe nome={item.nome} url={item.url} key={index}/>)}
-                </div>
-            </div> */}
-            
-            
-        </div>
+        <section className="project-762">
+            <div style={{textAlign:'center', paddingTop:30}}>
+                <Title title='Conheça nossos mission'/>
+            </div>
+            <div>
+                <Projeto title="RTAeasy" url="images/rta-easy.png" site="https://rta-easy-mocha.vercel.app/" content="O RTAeasy é um software desenvolvido para diversas plataformas. Com RTA você poderá calcular a expressão gênica com vários tratamentos de vários genes de uma vez! Relate seu experimento para o RTAeasy e ele monta a planilha para você e escolha entre os principais métodos utilizados, o delta delta CT (Livak) e Pfaffl."/>
+                <Projeto title="RGeasy" url="images/rgeasy-png.png" site="http://rgeasy.com.br/" content="O RGeasy tem por objetivo auxiliar na seleção de genes de referência, para estudos de expressão gênica po RT-qPCR de forma facilitada, ele utiliza o algoritmo RefFinder (XIE et al, 2012), para a classificação dos genes, resultando em um ranking com os genes de referência mais estáveis e, ainda, disponibiliza conjuntos de primers dos genes selecionados."/>
+            </div>
+        </section>
     )
 }
 
