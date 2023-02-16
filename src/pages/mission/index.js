507b3455a331565e4c3263a86784df7714842614
Lab-LAM <122761329+Lab-LAM@users.noreply.github.com>
2023-02-16 17:00:47 -0300

    Update index.js

--- src/pages/mission/index.js
+++ src/pages/mission/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Content } from "react-bootstrap/lib/Tab";
+import Conteudo from "../../components/Conteudo";
 import Title from "../../components/Title";
 import './mission.css';
 
@@ -6,7 +8,14 @@ const Missão = ()=>{
     return(
         <div className="mission">
             <div className="mission-title">
-                <Title title="Como você prefere falar com a gente?"/>
+                <Title title="titulo missão!!!"/>
+                <Conteudo Content=  "Cemeteries are just garbage dumps filled with humans. Como se escreve o número 0 em algarismos romanos?. Como se escreve o número 0 em algarismos romanos?. Onde quer que você esteja você sempre estará lá.. Agora, mais do que nunca, é a hora..
+
+Pobre só enche a barriga quando morre afogado.. Toda regra tem exceção.. Um é pouco, dois é bom e três é ímpar.. Qual é o diminutivo de golfinho?. Jesus salva! Que passa para Moisés, que chuta e é gooooolllll....
+
+You say potatoe, I say starchy carbs. É hoje que eu volto amanhã.. Diz-me com quem andas e te direi se vou contigo.. A vida é uma vai e vem que não tem volta.. Um é pouco, dois é bom e três é ímpar..
+
+I bet most serial killers play the drums. Uma coisa é certa: ninguém sai vivo dessa vida.. Pobre só enche a barriga quando morre afogado.. O dinheiro não traz felicidade, então, dê todo o seu pra mim e seja feliz.. Os últimos serão os primeiros e os do meio sempre serão os do meio.."/>
             </div>
         </div>
     )
